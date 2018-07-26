12deaeab0d7a866a1903c620d09aad3877a99ada
Lansefanggezi <lansefanggezi123@163.com>
2018-07-26 10:15:33 +0800

    闹钟程序-字符串比较

---
+++ WakeUp/src/com/kang/wakeup/FindStr.java
@@ -0,0 +1,33 @@
+package com.kang.wakeup;
+
+public class FindStr {
+
+	/**
+	 * 暴力查找一个字符串在另一个字符串中的位置
+	 */
+	public static void main(String[] args) {
+		System.out.println(FindStr.findStr("Spring", "ng"));
+	}
+
+	public static int findStr(String S, String T) {
+		// 指向主串，比较的位置
+		int i = 0;
+
+		// 指向字串,比较的次数
+		int j = 0;
+		while ((i < S.length()) && (j < T.length())) {
+			if (S.charAt(i) == T.charAt(j)) {
+				j++;
+				i++;
+			} else {
+				j = 0;
+				i = i - j + 1;
+			}
+		}
+		if (j == T.length()) {
+			return i - j + 1;
+		} else {
+			return -1;
+		}
+	}
+}
